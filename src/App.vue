fd374e61edbdd7a0325e05db143c1e3606286a0b
nnbzh <nurasyl.bzh@gmail.com>
2021-03-05 16:00:12 +0600

    display blobs from storage

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="(item, index) of images" :key="index" style="border:1px #000 solid;width:30%; margin:auto;">
+        {{item.name}}
+        <img :src="item.link" alt="" style="width:300px; height:300px;">
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      images: ""
+    }
+  },
+  mounted() {
+    let parser = new DOMParser();
+    let images = [];
+    let today = new Date();
+    today = today.toUTCString();
+    axios.get('https://mystorennbzh.blob.core.windows.net/images?sp=rl&st=2021-03-05T08:36:01Z&se=2021-03-19T16:36:01Z&spr=https&sv=2020-02-10&sr=c&sig=5S4tCseXl9KU%2Fm1Peirl%2FrURw%2FeqyD%2BOOrcr0RTxACk%3D&comp=list&restype=container',{
+      headers: {
+        'x-ms-date':today,
+        'x-ms-version': '2020-06-12'
+      }
+    }).then(function (response) {
+      let arr = parser.parseFromString(response.data, 'text/xml').getElementsByTagName('Blob');
+      arr.forEach(element => {
+          images.push({
+            name: element.firstChild.firstChild.data,
+            link: `https://mystorennbzh.blob.core.windows.net/images/${element.firstChild.firstChild.data}?sp=rl&st=2021-03-05T08:36:01Z&se=2021-03-19T16:36:01Z&spr=https&sv=2020-02-10&sr=c&sig=5S4tCseXl9KU%2Fm1Peirl%2FrURw%2FeqyD%2BOOrcr0RTxACk%3D`
+          });
+      });
+    });
+      this.images = images;
+      console.log(images);
   }
 }
 </script>
